2236e5b65725ba36580e0169005371c109306ac6
ChalkPE <chalk@chalk.pe>
2016-12-10 01:45:28 +0900

    Fixed errors

--- data/populate.js
+++ data/populate.js
@@ -2,6 +2,11 @@ const User = require('../models/User');
 const Article = require('../models/Article');
 
 module.exports = () => {
-    User.count({}).then(count => count || User.insertMany(require('./users')).then(() => console.log('Populated User models')));
-    Article.count({}).then(count => count || Article.insertMany(require('./articles')).then(() => console.log('Populated Article models')));
+    User.count({}).then(count => count || User.insertMany(require('./users'))
+        .then(() => console.log('Populated User models'))
+        .catch(err => console.log(err)));
+
+    Article.count({}).then(count => count || Article.insertMany(require('./articles'))
+        .then(() => console.log('Populated Article models'))
+        .catch(err => console.log(err)));
 };
