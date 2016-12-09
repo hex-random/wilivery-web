b809f7a1f50ff69d252381d483ed9d8b6d7e26af
ChalkPE <chalk@chalk.pe>
2016-12-09 22:27:15 +0900

    Added error page

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,15 @@ module.exports = (app, passport) => {
         req.logout();
         res.redirect('/');
     });
+
+    app.use((req, res, next) => {
+        let err = new Error('Not Found');
+        err.status = 404; next(err);
+    });
+
+
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.render('pages/error', { message: err.message, error: err });
+    });
 };
