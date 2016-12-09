9cc0576c4c43da8dff0858103a27dcd266f46eb1
ChalkPE <chalk@chalk.pe>
2016-12-09 17:34:40 +0900

    Added interestedIn field

--- models/Group.js
+++ models/Group.js
@@ -3,7 +3,7 @@ const User = require('./User');
 
 const schema = mongoose.Schema({
     name: { type: String, maxlength: 32, index: true, unique: true },
-    admin: User, clients: [User]
+    admin: User, clients: [User], interestedIn: [String]
 });
 
 module.exports = mongoose.model('Group', schema);
--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const bcryptjs = require('bcryptjs');
 
 const schema = mongoose.Schema({
     email: { type: String, index: true, unique: true },
-    password: { type: String, set: bcryptjs.hashSync }
+    password: { type: String, set: bcryptjs.hashSync },
+    interestedIn: [String]
 });
 
 module.exports = mongoose.model('User', schema);
